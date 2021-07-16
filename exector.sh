8949bb21e593a0423aef2ba9b4939e0a06b7dcf2
issam elyazidi <ie.elyazidi@gmail.com>
2021-07-16 17:12:27 +0100

    Generating of file

---
+++ exector.sh
@@ -0,0 +1,12 @@
+split -l 200 $1 splited_files/ 
+node app.js
+
+cd ./generated_json
+
+for file in *; do
+    gsutil cp "$file" gs://expedia-static-data
+    bq load --source_format=NEWLINE_DELIMITED_JSON suppliers_dashboard._expedia_static_data_test gs://expedia-static-data/"$file"
+done
+
+
+
